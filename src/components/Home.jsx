4e2e25a3b4abb0ac20f3dec526ce9d8b4f04800f
Badrouu17 <badrouu.laabed@gmail.com>
2020-05-02 01:02:52 +0100

    fix home buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,12 +19,18 @@ const Home = () => {
         <p className="lobster-font" style={{ fontSize: 30 }}>
           the worlds symbol of love ❤ share them.
         </p>
-        <button className="w-3/4 h-16 text-2xl mt-16 py-2 px-4 bg-transparent font-bold text-grey-100 hover:text-red-600 hover:bg-gray-100 border-2 border-solid border-gray-100 rounded-full">
-          <Link to="/login">L O G I N</Link>
-        </button>
-        <button className="w-3/4 h-16 text-2xl mt-8 py-2 px-4 bg-transparent font-bold text-grey-100 hover:text-red-600 hover:bg-gray-100 border-2 border-solid border-gray-100 rounded-full">
-          <Link to="/signup">S I G N U P</Link>
-        </button>
+        <Link
+          className="w-3/4 h-16 text-2xl mt-8 py-2 px-4 bg-transparent font-bold text-grey-100 hover:text-red-600 hover:bg-gray-100 border-2 border-solid border-gray-100 rounded-full"
+          to="/login"
+        >
+          L O G I N
+        </Link>
+        <Link
+          className="w-3/4 h-16 text-2xl mt-8 py-2 px-4 bg-transparent font-bold text-grey-100 hover:text-red-600 hover:bg-gray-100 border-2 border-solid border-gray-100 rounded-full"
+          to="/signup"
+        >
+          S I G N U P
+        </Link>
       </div>
     </ContainerSmall>
   );
